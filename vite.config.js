bb6e37691da5d1fe53613aab5aa57eb89a4e70c8
ravisaitejakuna <88229369+ravisaitejakuna@users.noreply.github.com>
2025-10-07 16:41:59 -0700

    Final working build ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ravi-portfolio/',  // 👈 VERY IMPORTANT (must match your repo name)
+  base: './', // 👈 Use relative paths for local + GitHub Pages compatibility
 })
