5e24edb35d5e517b108e1b3736df12621a9b6984
Ravisaitejakuna <ravisaitejakuna@gmail.com>
2026-05-28 23:46:12 -0400

    Update portfolio with new projects and images

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Use relative paths for local + GitHub Pages compatibility
+  base: '/ravi-portfolio/',
 })
